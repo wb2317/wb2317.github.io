45fc3ccf5990e328831fd759163ac97e5064c1b7
Weijian B <wb2317@columbia.edu>
2018-01-24 14:59:56 -0500

    Hello World Assignment 1

--- sketch.js
+++ sketch.js
@@ -12,10 +12,10 @@ function draw() {
   //righteye
   fill(0,0,0);
   stroke(0,0,0);
-  ellipse(mouseX+200,mouseY+150,30,30);
+  ellipse(200,150,30,30);
   //lefteye
   fill(0,0,0);
-  ellipse(mouseX+300,mouseY+150,30,30);
+  ellipse(300,150,30,30);
   //line between left and right eye
   fill(0,0,0);
   strokeWeight(2); 
@@ -23,19 +23,19 @@ function draw() {
   //leftblush
   fill(255,201,160);
   stroke(255,201,160);
-  ellipse(mouseX+180,mouseY+180,20,10);
+  ellipse(180,180,20,10);
   //rightblush
   fill(255,201,160);
   stroke(255,201,160);
-  ellipse(mouseX+320,mouseY+180,20,10);
+  ellipse(320,180,20,10);
   //body
   fill(252,252,252);
   stroke(252,252,252);
-  ellipse(mouseX+250,mouseY+400,380,400);
+  ellipse(250,400,380,400);
   //port
   fill(252,252,252);
   stroke(0,0,0);
-  ellipse(mouseX+340,mouseY+300,50,50);
+  ellipse(340,300,50,50);
   line(317,300,330,300);
   line(330,300,330,290);
   line(330,290,350,290);
@@ -43,9 +43,10 @@ function draw() {
   line(350,300,365,300);
   //text
   fill(252,252,252);
-  textSize(25)
+  textSize(25);
   text("Hello World!",340,100);
  }
+ 
  
 
 
