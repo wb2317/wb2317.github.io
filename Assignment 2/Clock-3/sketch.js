c524ceaa6416c04b094e02c26acbcf8a330816b5
Weijian B <wb2317@columbia.edu>
2018-01-31 17:39:48 -0500

    Clock-3

--- Assignment 2/Clock-3/sketch.js
+++ Assignment 2/Clock-3/sketch.js
@@ -1,50 +1,33 @@
  function setup() {
-   createCanvas(400, 400);
+   createCanvas(480, 480);
    angleMode(DEGREES);
  }
 
  function draw() {
-   background(0);
-   translate(200, 200);
-   rotate(-90);
+   background(204);   
 
-   let hr = hour();
-   let mn = minute();
-   let sc = second();
+   var h = hour();
+   var m = minute();
+   var s = second();
+  
+   strokeWeight();
+   fill(255,0,0,150)
+   arc1 = map(s, 0, 60, 0, 360);
+   triangle(width/2, height/2, 300, 300, 0, arc1); 
 
-   strokeWeight(8);
-   stroke(255, 100, 150);
-   noFill();
-   let secondAngle = map(sc, 0, 60, 0, 360);
-   //arc(0, 0, 300, 300, 0, secondAngle);
 
-   stroke(150, 100, 255);
-   let minuteAngle = map(mn, 0, 60, 0, 360);
-   //arc(0, 0, 280, 280, 0, minuteAngle);
+   fill(255, 255, 0,150);
+   arc2 = map(m, 0, 60, 0, 360);
+   triangle(width/2, height/2, 280, 280, 0, arc2);
 
-   stroke(150, 255, 100);
-   let hourAngle = map(hr % 12, 0, 12, 0, 360);
-   //arc(0, 0, 260, 260, 0, hourAngle);
 
-   push();
-   rotate(secondAngle);
-   stroke(255, 100, 150);
-   line(0, 0, 100, 0);
-   pop();
+   fill(0, 0, 250,150);
+   arc3 = map(h, 0, 12, 0, 360);
+   triangle(width/2, height/2, 260, 260, 0, arc3);
+  
+   fill(255);
+   noStroke();
+   text('The time right now is :\n'+ h + ':' + m + ':' + s, 160,50);
 
-   push();
-   rotate(minuteAngle);
-   stroke(150, 100, 255);
-   line(0, 0, 75, 0);
-   pop();
-
-   push();
-   rotate(hourAngle);
-   stroke(150, 255, 100);
-   line(0, 0, 50, 0);
-   pop();
-
-   stroke(255);
-   point(0, 0);
-
-}
+ }
+ 
